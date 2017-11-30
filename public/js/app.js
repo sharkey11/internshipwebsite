53bce0ef689c143b64cad3984015fafd4d866512
Jack Sharkey <sharkeyjack11@gmail.com>
2017-11-29 22:08:05 -0500

    added google oauth and ability to add users to db

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,29 @@
-console.log("test")
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log("hey")
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 
-$(".sidebar").click(function(){
-    $('.ui.sidebar')
-    .sidebar('toggle')
-  ;
-})
+
+  var data = {};
+  data.name = profile.getName();
+  data.id = profile.getId();
+  data.email = profile.getEmail();
+
+
+  $.ajax({
+    type: 'POST',
+    url: '/login',
+    data: data,
+    success: function(data) {
+
+     console.log("success")
+//change to authorized.html
+
+      },
+      error: function(e) {
+       console.log(e)
+      }
+});
+}
